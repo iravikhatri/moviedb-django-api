764deb10375903d604bbd9e4007a55f97b4ab35d
Pawan <pawanpreet@talentmarketplace.ca>
2021-06-30 15:43:45 +0530

    Token setted

--- moviedb/settings.py
+++ moviedb/settings.py
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'moviedb.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'moviedb_database',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moviedb_staging',
+    }
 }
 
 AUTH_USER_MODEL = 'accounts.User'
